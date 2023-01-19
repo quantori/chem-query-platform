471481604ca5718ff2f4dd37c8f7b7b9ce625fd0
Artem.Chukin <artem.chukin@quantori.com>
2023-01-19 16:30:39 +0400

    QDP-85 Unify UploadSource and SearchIterators in QDP core and QDP API

--- qdp-storage/api/src/main/java/com/quantori/qdp/api/model/core/DataStorage.java
+++ qdp-storage/api/src/main/java/com/quantori/qdp/api/model/core/DataStorage.java
@@ -1,13 +1,15 @@
 package com.quantori.qdp.api.model.core;
 
+import com.quantori.qdp.api.service.ItemWriter;
+import com.quantori.qdp.api.service.SearchIterator;
 import java.util.List;
 
 public interface DataStorage<U extends StorageUploadItem, S extends SearchItem, I extends StorageItem> {
-  default DataLoader<U> dataLoader(String libraryId) {
+  default ItemWriter<U> itemWriter(String libraryId) {
     throw new UnsupportedOperationException();
   }
 
-  default List<DataSearcher<I>> dataSearcher(RequestStructure<S, I> storageRequest) {
+  default List<SearchIterator<I>> searchIterator(RequestStructure<S, I> storageRequest) {
     throw new UnsupportedOperationException();
   }
 }
