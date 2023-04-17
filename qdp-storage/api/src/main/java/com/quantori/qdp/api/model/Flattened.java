c17fba6d2794c06d40fc7a38ad25ad07ac4add1b
Vyacheslav Teryaev <vyacheslav.teryaev@quantori.com>
2023-04-17 18:20:58 +0400

    [QDP-113] Allow flattened molecule to be saved again

--- qdp-storage/api/src/main/java/com/quantori/qdp/api/model/Flattened.java
+++ qdp-storage/api/src/main/java/com/quantori/qdp/api/model/Flattened.java
@@ -3,9 +3,9 @@ package com.quantori.qdp.api.model;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.quantori.qdp.api.model.core.SearchItem;
 import com.quantori.qdp.api.model.core.StorageItem;
-import java.time.Instant;
-import java.util.Map;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 /**
  * Flattened search result structure.
@@ -16,46 +16,33 @@ public interface Flattened {
    * Molecule/Compound.
    */
   @Data
+  @ToString(callSuper = true)
+  @EqualsAndHashCode(callSuper = true)
   @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, defaultImpl = Molecule.class)
-  class Molecule implements SearchItem, StorageItem {
-    private String id;
-    private String smiles;
-    private byte[] structure;
-    private Map<String, String> properties;
+  class Molecule extends com.quantori.qdp.api.model.upload.Molecule implements SearchItem, StorageItem {
     private String libraryId;
     private String libraryName;
     private String storageType;
-    private Long customOrder;
-    private Instant createdStamp;
-    private Instant updatedStamp;
   }
 
   /**
    * Reaction.
    */
   @Data
+  @ToString(callSuper = true)
+  @EqualsAndHashCode(callSuper = true)
   @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, defaultImpl = Reaction.class)
-  class Reaction implements SearchItem, StorageItem {
-    private String id;
-    private String smiles;
-    private String paragraphText;
-    private String amount;
-    private String source;
-    private String reactionDocumentId;
+  class Reaction extends com.quantori.qdp.api.model.upload.Reaction implements SearchItem, StorageItem {
     private String libraryId;
+    private String libraryName;
     private String storageType;
-    private Instant createdStamp;
-    private Instant updatedStamp;
   }
 
   @Data
+  @ToString(callSuper = true)
+  @EqualsAndHashCode(callSuper = true)
   @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, defaultImpl = ReactionParticipant.class)
-  class ReactionParticipant {
-    private String id;
-    private String name;
-    private String smiles;
-    private String inchi;
-    private ReactionParticipantRole role;
-    private String type;
+  class ReactionParticipant extends com.quantori.qdp.api.model.upload.ReactionParticipant
+      implements SearchItem, StorageItem {
   }
 }
