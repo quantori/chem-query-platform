8d9effb93c54890125157bf89f331bcece4b5e20
Giorgi Gagnidze <giorgi.gagnidze@quantori.com>
2023-06-13 14:30:30 +0400

    Format code

--- qdp-storage/api/src/main/java/com/quantori/qdp/api/model/SortParams.java
+++ qdp-storage/api/src/main/java/com/quantori/qdp/api/model/SortParams.java
@@ -3,14 +3,14 @@ package com.quantori.qdp.api.model;
 import java.util.List;
 
 public record SortParams(List<Sort> sortList) {
-    public static SortParams ofSortList(List<Sort> sortList) {
-        return new SortParams(sortList);
-    }
+  public static SortParams ofSortList(List<Sort> sortList) {
+    return new SortParams(sortList);
+  }
 
-    public enum Type {GENERAL, NESTED}
+  public enum Type {GENERAL, NESTED}
 
-    public enum Order {ASC, DESC}
+  public enum Order {ASC, DESC}
 
-    public record Sort(String fieldName, Order order, Type type) {
-    }
+  public record Sort(String fieldName, Order order, Type type) {
+  }
 }
