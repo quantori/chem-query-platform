847d22403a9dd32b7ce30c675177d380f9129a0a
simar mugattarov <simar.mugattarov@quantori.com>
2024-06-20 17:53:48 +0500

    hotfix: remove method taskCompleted
    as data provide has nothing todo with "task" of "completed something" method is removed.

--- qdp-core/src/main/java/com/quantori/qdp/core/task/model/DataProvider.java
+++ qdp-core/src/main/java/com/quantori/qdp/core/task/model/DataProvider.java
@@ -5,14 +5,12 @@ import java.util.Iterator;
 import java.util.List;
 
 public interface DataProvider {
-
+    Data EMPTY_DATA = new Data() { };
     DataProvider EMPTY = Collections::emptyIterator;
 
     Iterator<? extends Data> dataIterator();
 
     default void close() { }
-
-    default void taskCompleted(boolean successful) { }
 
     interface Data { }
 }
